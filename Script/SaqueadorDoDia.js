942ea0326287a62327b10d07763ca5fcd930477a
Perin <joaovperin@live.com>
2020-05-28 14:37:18 -0300

    Otimizado função que verifica se há recaptcha!

--- Script/SaqueadorDoDia.js
+++ Script/SaqueadorDoDia.js
@@ -14,6 +14,9 @@ $(document).ready(function () {
 
         function isRecaptchaWindowPresent() {
             var isPresent = false;
+            if (document.querySelector('#bot_check')) {
+                isPresent = true;
+            }
             if (document.querySelector('#rc-anchor-alert')) {
                 isPresent = true;
             }
@@ -21,6 +24,9 @@ $(document).ready(function () {
                 isPresent = true;
             }
             if (document.querySelector("#recaptcha-token")) {
+                isPresent = true;
+            }
+            if ($(':contains(Proteção contra Bots)').val() !== undefined) {
                 isPresent = true;
             }
             if (isPresent) {
