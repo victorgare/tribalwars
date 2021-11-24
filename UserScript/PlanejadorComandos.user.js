c2553bcb8be8859966d2a2018b44c739dec6fe53
Victor Rodrigues <garevictor@hotmail.com>
2021-11-24 13:36:54 -0300

    Corrigido planejador de comandos

--- UserScript/PlanejadorComandos.user.js
+++ UserScript/PlanejadorComandos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         150 - Planejador XPTO 
 // @author       Vende se
-// @version      0.1
+// @version      0.2
 // @include      https://*screen=place&try=confirm*
 // @require https://code.jquery.com/jquery-2.2.4.min.js
 // @downloadURL https://raw.githubusercontent.com/victorgare/tribalwars/master/UserScript/PlanejadorComandos.user.js
@@ -9,82 +9,81 @@
 // @run-at       document-start
 // ==/UserScript==
 
-var _0x9780 = ["\x3C\x74\x72\x3E\x3C\x74\x64\x3E\x43\x68\x65\x67\x61\x64\x61\x3A\x3C\x2F\x74\x64\x3E\x3C\x74\x64\x3E\x20\x3C\x69\x6E\x70\x75\x74\x20\x74\x79\x70\x65\x3D\x22\x64\x61\x74\x65\x74\x69\x6D\x65\x2D\x6C\x6F\x63\x61\x6C\x22\x20\x69\x64\x3D\x22\x43\x53\x74\x69\x6D\x65\x22\x20\x73\x74\x65\x70\x3D\x22\x2E\x30\x30\x31\x22\x3E\x20\x3C\x2F\x74\x64\x3E\x3C\x2F\x74\x72\x3E\x3C\x74\x72\x3E\x20\x3C\x74\x64\x3E\x4F\x66\x66\x73\x65\x74\x3A\x3C\x2F\x74\x64\x3E\x3C\x74\x64\x3E\x20\x3C\x69\x6E\x70\x75\x74\x20\x74\x79\x70\x65\x3D\x22\x6E\x75\x6D\x62\x65\x72\x22\x20\x69\x64\x3D\x22\x43\x53\x6F\x66\x66\x73\x65\x74\x22\x3E\x20\x3C\x62\x75\x74\x74\x6F\x6E\x20\x74\x79\x70\x65\x3D\x22\x62\x75\x74\x74\x6F\x6E\x22\x20\x69\x64\x3D\x22\x43\x53\x62\x75\x74\x74\x6F\x6E\x22\x20\x63\x6C\x61\x73\x73\x3D\x22\x62\x74\x6E\x22\x3E\x43\x6F\x6E\x66\x69\x72\x6D\x61\x72\x3C\x2F\x62\x75\x74\x74\x6F\x6E\x3E\x20\x3C\x2F\x74\x64\x3E\x3C\x2F\x74\x72\x3E", "\x61\x70\x70\x65\x6E\x64", "\x74\x62\x6F\x64\x79", "\x66\x69\x6E\x64", "\x23\x63\x6F\x6D\x6D\x61\x6E\x64\x2D\x64\x61\x74\x61\x2D\x66\x6F\x72\x6D", "\x63\x6F\x6E\x66\x69\x72\x6D\x42\x75\x74\x74\x6F\x6E", "\x23\x74\x72\x6F\x6F\x70\x5F\x63\x6F\x6E\x66\x69\x72\x6D\x5F\x67\x6F", "\x64\x75\x72\x61\x74\x69\x6F\x6E", "\x6D\x61\x70", "\x3A", "\x73\x70\x6C\x69\x74", "\x74\x65\x78\x74", "\x6E\x65\x78\x74", "\x74\x64\x3A\x63\x6F\x6E\x74\x61\x69\x6E\x73\x28\x22\x44\x75\x72\x61\xE7\xE3\x6F\x3A\x22\x29", "\x6F\x66\x66\x73\x65\x74", "\x43\x53\x2E\x6F\x66\x66\x73\x65\x74", "\x67\x65\x74\x49\x74\x65\x6D", "\x64\x61\x74\x65\x4E\x6F\x77", "\x63\x6F\x6E\x76\x65\x72\x74\x54\x6F\x49\x6E\x70\x75\x74", "\x76\x61\x6C", "\x23\x43\x53\x6F\x66\x66\x73\x65\x74", "\x23\x43\x53\x74\x69\x6D\x65", "\x67\x65\x74\x41\x74\x74\x61\x63\x6B\x54\x69\x6D\x65", "\x73\x65\x74\x49\x74\x65\x6D", "\x62\x74\x6E\x2D\x64\x69\x73\x61\x62\x6C\x65\x64", "\x61\x64\x64\x43\x6C\x61\x73\x73", "\x63\x6C\x69\x63\x6B", "\x67\x65\x74\x43\x75\x72\x72\x65\x6E\x74\x53\x65\x72\x76\x65\x72\x54\x69\x6D\x65", "\x64\x69\x73\x61\x62\x6C\x65\x64", "\x23\x43\x53\x62\x75\x74\x74\x6F\x6E", "\x54", "\x20", "\x72\x65\x70\x6C\x61\x63\x65", "\x67\x65\x74\x48\x6F\x75\x72\x73", "\x73\x65\x74\x48\x6F\x75\x72\x73", "\x67\x65\x74\x4D\x69\x6E\x75\x74\x65\x73", "\x73\x65\x74\x4D\x69\x6E\x75\x74\x65\x73", "\x67\x65\x74\x53\x65\x63\x6F\x6E\x64\x73", "\x73\x65\x74\x53\x65\x63\x6F\x6E\x64\x73", "\x67\x65\x74\x46\x75\x6C\x6C\x59\x65\x61\x72", "\x67\x65\x74\x4D\x6F\x6E\x74\x68", "\x67\x65\x74\x44\x61\x74\x65", "\x74\x6F\x54\x69\x6D\x65\x53\x74\x72\x69\x6E\x67", "\x67\x65\x74\x4D\x69\x6C\x6C\x69\x73\x65\x63\x6F\x6E\x64\x73", "\x6D", "\x30", "\x64", "\x6D\x73", "\x79", "\x2D", "\x74\x69\x6D\x65", "\x2E", "\x68\x65\x61\x64", "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65", "\x73\x74\x79\x6C\x65", "\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74", "\x74\x79\x70\x65", "\x74\x65\x78\x74\x2F\x63\x73\x73", "\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C", "\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64", "\x23\x43\x53\x74\x69\x6D\x65\x2C\x20\x23\x43\x53\x6F\x66\x66\x73\x65\x74\x20\x7B\x66\x6F\x6E\x74\x2D\x73\x69\x7A\x65\x3A\x20\x39\x70\x74\x3B\x66\x6F\x6E\x74\x2D\x66\x61\x6D\x69\x6C\x79\x3A\x20\x56\x65\x72\x64\x61\x6E\x61\x2C\x41\x72\x69\x61\x6C\x3B\x7D\x23\x43\x53\x62\x75\x74\x74\x6F\x6E\x20\x7B\x66\x6C\x6F\x61\x74\x3A\x72\x69\x67\x68\x74\x3B\x7D", "\x61\x64\x64\x47\x6C\x6F\x62\x61\x6C\x53\x74\x79\x6C\x65", "\x63\x6F\x6D\x6D\x61\x6E\x64\x2D\x64\x61\x74\x61\x2D\x66\x6F\x72\x6D", "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64", "\x69\x6E\x69\x74", "\x6F\x66\x66\x73\x65\x74\x5F\x74\x6F\x5F\x73\x65\x72\x76\x65\x72", "\x65\x71"];
 CommandSender = {
     confirmButton: null,
     duration: null,
     dateNow: null,
     offset: null,
-    init: function () {
-        $($(_0x9780[4])[_0x9780[3]](_0x9780[2])[0])[_0x9780[1]](_0x9780[0]);
-        this[_0x9780[5]] = $(_0x9780[6]);
-        this[_0x9780[7]] = $(_0x9780[4])[_0x9780[3]](_0x9780[13])[_0x9780[12]]()[_0x9780[11]]()[_0x9780[10]](_0x9780[9])[_0x9780[8]](Number);
-        this[_0x9780[14]] = localStorage[_0x9780[16]](_0x9780[15]) || -250;
-        this[_0x9780[17]] = this[_0x9780[18]](new Date());
-        $(_0x9780[20])[_0x9780[19]](this[_0x9780[14]]);
-        $(_0x9780[21])[_0x9780[19]](this[_0x9780[17]]);
-        $(_0x9780[29])[_0x9780[26]](function () {
-            var _0xbbd0x1 = Number($(_0x9780[20])[_0x9780[19]]());
-            var _0xbbd0x2 = CommandSender[_0x9780[22]]();
-            localStorage[_0x9780[23]](_0x9780[15], _0xbbd0x1);
-            CommandSender[_0x9780[5]][_0x9780[25]](_0x9780[24]);
-            setTimeout(function () {
-                CommandSender[_0x9780[5]][_0x9780[26]]()
-            }, _0xbbd0x2 - Timing[_0x9780[27]]() + _0xbbd0x1);
-            this[_0x9780[28]] = true
+    init: function() {
+        $($('#command-data-form')['find']('tbody')[0])['append']('<tr><td>Chegada:</td><td> <input type="datetime-local" id="CStime" step=".001"> </td></tr><tr> <td>Offset:</td><td> <input type="number" id="CSoffset"> <button type="button" id="CSbutton" class="btn">Confirmar</button> </td></tr>');
+        this['confirmButton'] = $('#troop_confirm_submit');
+        this['duration'] = $('#command-data-form')['find']('td:contains("Dura\xE7\xE3o:")')['next']()['text']()['split'](':')['map'](Number);
+        this['offset'] = localStorage['getItem']('CS.offset') || -250;
+        this['dateNow'] = this['convertToInput'](new Date());
+        $('#CSoffset')['val'](this['offset']);
+        $('#CStime')['val'](this['dateNow']);
+        $('#CSbutton')['click'](function() {
+            var _0xbbd0x1 = Number($('#CSoffset')['val']());
+            var _0xbbd0x2 = CommandSender['getAttackTime']();
+            localStorage['setItem']('CS.offset', _0xbbd0x1);
+            CommandSender['confirmButton']['addClass']('btn-disabled');
+            setTimeout(function() {
+                CommandSender['confirmButton']['click']()
+            }, _0xbbd0x2 - Timing['getCurrentServerTime']() + _0xbbd0x1);
+            this['disabled'] = true
         })
     },
-    getAttackTime: function () {
-        var _0xbbd0x3 = new Date($(_0x9780[21])[_0x9780[19]]()[_0x9780[32]](_0x9780[30], _0x9780[31]));
-        _0xbbd0x3[_0x9780[34]](_0xbbd0x3[_0x9780[33]]() - this[_0x9780[7]][0]);
-        _0xbbd0x3[_0x9780[36]](_0xbbd0x3[_0x9780[35]]() - this[_0x9780[7]][1]);
-        _0xbbd0x3[_0x9780[38]](_0xbbd0x3[_0x9780[37]]() - this[_0x9780[7]][2]);
+    getAttackTime: function() {
+        var _0xbbd0x3 = new Date($('#CStime')['val']()['replace']('T', ' '));
+        _0xbbd0x3['setHours'](_0xbbd0x3['getHours']() - this['duration'][0]);
+        _0xbbd0x3['setMinutes'](_0xbbd0x3['getMinutes']() - this['duration'][1]);
+        _0xbbd0x3['setSeconds'](_0xbbd0x3['getSeconds']() - this['duration'][2]);
         return _0xbbd0x3
     },
-    convertToInput: function (_0xbbd0x4) {
-        _0xbbd0x4[_0x9780[34]](_0xbbd0x4[_0x9780[33]]() + this[_0x9780[7]][0]);
-        _0xbbd0x4[_0x9780[36]](_0xbbd0x4[_0x9780[35]]() + this[_0x9780[7]][1]);
-        _0xbbd0x4[_0x9780[38]](_0xbbd0x4[_0x9780[37]]() + this[_0x9780[7]][2]);
+    convertToInput: function(_0xbbd0x4) {
+        _0xbbd0x4['setHours'](_0xbbd0x4['getHours']() + this['duration'][0]);
+        _0xbbd0x4['setMinutes'](_0xbbd0x4['getMinutes']() + this['duration'][1]);
+        _0xbbd0x4['setSeconds'](_0xbbd0x4['getSeconds']() + this['duration'][2]);
         var a = {
-            y: _0xbbd0x4[_0x9780[39]](),
-            m: _0xbbd0x4[_0x9780[40]]() + 1,
-            d: _0xbbd0x4[_0x9780[41]](),
-            time: _0xbbd0x4[_0x9780[42]]()[_0x9780[10]](_0x9780[31])[0],
-            ms: _0xbbd0x4[_0x9780[43]]()
+            y: _0xbbd0x4['getFullYear'](),
+            m: _0xbbd0x4['getMonth']() + 1,
+            d: _0xbbd0x4['getDate'](),
+            time: _0xbbd0x4['toTimeString']()['split'](' ')[0],
+            ms: _0xbbd0x4['getMilliseconds']()
         };
-        if (a[_0x9780[44]] < 10) {
-            a[_0x9780[44]] = _0x9780[45] + a[_0x9780[44]]
+        if (a['m'] < 10) {
+            a['m'] = '0' + a['m']
         };
-        if (a[_0x9780[46]] < 10) {
-            a[_0x9780[46]] = _0x9780[45] + a[_0x9780[46]]
+        if (a['d'] < 10) {
+            a['d'] = '0' + a['d']
         };
-        if (a[_0x9780[47]] < 100) {
-            a[_0x9780[47]] = _0x9780[45] + a[_0x9780[47]];
-            if (a[_0x9780[47]] < 10) {
-                a[_0x9780[47]] = _0x9780[45] + a[_0x9780[47]]
+        if (a['ms'] < 100) {
+            a['ms'] = '0' + a['ms'];
+            if (a['ms'] < 10) {
+                a['ms'] = '0' + a['ms']
             }
         };
-        return a[_0x9780[48]] + _0x9780[49] + a[_0x9780[44]] + _0x9780[49] + a[_0x9780[46]] + _0x9780[30] + a[_0x9780[50]] + _0x9780[51] + a[_0x9780[47]]
+        return a['y'] + '-' + a['m'] + '-' + a['d'] + 'T' + a['time'] + '.' + a['ms']
     },
-    addGlobalStyle: function (_0xbbd0x6) {
+    addGlobalStyle: function(_0xbbd0x6) {
         var _0xbbd0x7, _0xbbd0x8;
-        _0xbbd0x7 = document[_0x9780[53]](_0x9780[52])[0];
+        _0xbbd0x7 = document['getElementsByTagName']('head')[0];
         if (!_0xbbd0x7) {
             return
         };
-        _0xbbd0x8 = document[_0x9780[55]](_0x9780[54]);
-        _0xbbd0x8[_0x9780[56]] = _0x9780[57];
-        _0xbbd0x8[_0x9780[58]] = _0xbbd0x6;
-        _0xbbd0x7[_0x9780[59]](_0xbbd0x8)
+        _0xbbd0x8 = document['createElement']('style');
+        _0xbbd0x8['type'] = 'text/css';
+        _0xbbd0x8['innerHTML'] = _0xbbd0x6;
+        _0xbbd0x7['appendChild'](_0xbbd0x8)
     }
 };
-CommandSender[_0x9780[61]](_0x9780[60]);
-var a = setInterval(function () {
-    if (document[_0x9780[63]](_0x9780[62]) && jQuery) {
-        CommandSender[_0x9780[64]]();
+CommandSender['addGlobalStyle']('#CStime, #CSoffset {font-size: 9pt;font-family: Verdana,Arial;}#CSbutton {float:right;}');
+var a = setInterval(function() {
+    if (document['getElementById']('command-data-form') && jQuery) {
+        CommandSender['init']();
         clearInterval(a)
     }
 }, 1);
-setTimeout(function () {
-    $(_0x9780[20])[_0x9780[66]](0)[_0x9780[19]](_0x9780[49] + Timing[_0x9780[65]])
+setTimeout(function() {
+    $('#CSoffset')['eq'](0)['val']('-' + Timing['offset_to_server'])
 }, 2000)
