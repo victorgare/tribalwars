3848faa95a1afa83011b49f2e0d276906cf870d0
Victor Rodrigues <garevictor@hotmail.com>
2023-11-06 11:58:55 +0000

    Revert "fix: script running in background"
    This reverts commit 03e131dcbe6f4fc2759eb19c07c949c4556d8951.

--- Script/SmartFarm.js
+++ Script/SmartFarm.js
@@ -1,115 +1,122 @@
-const SmartFarm = new (function () {
-  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+(function () {
+  "use strict";
 
-  const randonTime = (superior, inferior) => {
-    const numPosibilidades = superior - inferior;
-    const aleat = Math.random() * numPosibilidades;
-    return Math.round(parseInt(inferior) + aleat);
-  };
+  const SmartFarm = new (function () {
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-  const getTemplates = () => {
-    return Accountmanager.farm.templates;
-  };
+    const randonTime = (superior, inferior) => {
+      const numPosibilidades = superior - inferior;
+      const aleat = Math.random() * numPosibilidades;
+      return Math.round(parseInt(inferior) + aleat);
+    };
 
-  const getCurrentUnits = () => {
-    return Accountmanager.farm.current_units;
-  };
+    const getTemplates = () => {
+      return Accountmanager.farm.templates;
+    };
 
-  const getAvailableLightCavalry = () => {
-    return Accountmanager.farm.current_units.light;
-    //   return parseInt(document.getElementById("light").innerHTML);
-  };
+    const getCurrentUnits = () => {
+      return Accountmanager.farm.current_units;
+    };
 
-  const getNextVillage = () => {
-    // query only rows that are visible!
-    return document.querySelector(
-      "tr[id^='village_']:not([style='display: none;'])"
-    );
-  };
+    const getAvailableLightCavalry = () => {
+      return Accountmanager.farm.current_units.light;
+      //   return parseInt(document.getElementById("light").innerHTML);
+    };
 
-  const hasLootedAll = (element) => {
-    const lastLoot = element.querySelector("img[src*='max_loot']");
-    return lastLoot.getAttribute("src").endsWith("1.png");
-  };
+    const getNextVillage = () => {
+      // query only rows that are visible!
+      return document.querySelector(
+        "tr[id^='village_']:not([style='display: none;'])"
+      );
+    };
 
-  const hasEnoughUnitsInTemplate = (template) => {
-    const units = getCurrentUnits();
+    const hasLootedAll = (element) => {
+      const lastLoot = element.querySelector("img[src*='max_loot']");
+      return lastLoot.getAttribute("src").endsWith("1.png");
+    };
 
-    for (const unitName in units) {
-      const unitQuantity = units[unitName];
-      const templateUnityQuantity = template[unitName];
+    const hasEnoughUnitsInTemplate = (template) => {
+      const units = getCurrentUnits();
 
-      if (templateUnityQuantity && unitQuantity < templateUnityQuantity) {
-        return false;
+      for (const unitName in units) {
+        const unitQuantity = units[unitName];
+        const templateUnityQuantity = template[unitName];
+
+        if (templateUnityQuantity && unitQuantity < templateUnityQuantity) {
+          return false;
+        }
       }
-    }
 
-    return true;
-  };
-
-  const clickTemplateB = (element) => {
-    element.querySelector("a.farm_icon.farm_icon_b").click();
-  };
-
-  const clickTemplateA = (element) => {
-    element.querySelector("a.farm_icon.farm_icon_a").click();
-  };
-
-  const validateAndSendTemplateA = (template, element) => {
-    if (hasEnoughUnitsInTemplate(template)) {
-      clickTemplateA(element);
       return true;
-    }
-    return false;
-  };
+    };
 
-  const reloadPage = () => {
-    // reload between 1 and 2 minutes
-    const reloadTime = randonTime(60000, 120000);
-    console.log(`will reload in ${reloadTime / 1000} seconds`);
-    setInterval(function () {
-      console.log("reloading...");
-      window.location.reload(true);
-    }, reloadTime);
-  };
+    const clickTemplateB = (element) => {
+      element.querySelector("a.farm_icon.farm_icon_b").click();
+    };
 
-  this.init = async () => {
-    let exit = false;
-    const [templateA, templateB] = Object.values(getTemplates());
+    const clickTemplateA = (element) => {
+      element.querySelector("a.farm_icon.farm_icon_a").click();
+    };
 
-    do {
-      const element = getNextVillage();
-
-      if (!element) {
-        exit = true;
-        console.log("no more villages to attack");
-        break;
+    const validateAndSendTemplateA = (template, element) => {
+      if (hasEnoughUnitsInTemplate(template)) {
+        clickTemplateA(element);
+        return true;
       }
+      return false;
+    };
 
-      if (hasLootedAll(element)) {
-        if (hasEnoughUnitsInTemplate(templateB)) {
-          clickTemplateB(element);
+    const reloadPage = () => {
+      // reload between 1 and 2 minutes
+      const reloadTime = randonTime(60000, 120000);
+      console.log(`will reload in ${reloadTime / 1000} seconds`);
+      setInterval(function () {
+        console.log("reloading...");
+        window.location.reload(true);
+      }, reloadTime);
+    };
+
+    this.init = async () => {
+      let exit = false;
+      const [templateA, templateB] = Object.values(getTemplates());
+
+      do {
+        const element = getNextVillage();
+
+        if (!element) {
+          exit = true;
+          console.log("no more villages to attack");
+          break;
+        }
+
+        if (hasLootedAll(element)) {
+          if (hasEnoughUnitsInTemplate(templateB)) {
+            clickTemplateB(element);
+          } else {
+            exit = !validateAndSendTemplateA(templateA, element);
+          }
         } else {
           exit = !validateAndSendTemplateA(templateA, element);
         }
-      } else {
-        exit = !validateAndSendTemplateA(templateA, element);
-      }
 
-      // await at leat 250 ms until next atak
-      // this prevent request flood
-      const waitTime = randonTime(250, 350);
-      await delay(waitTime);
+        // await at leat 250 ms until next atak
+        // this prevent request flood
+        const waitTime = randonTime(250, 350);
+        await delay(waitTime);
 
-      if (exit) {
-        console.log("exiting...");
-      }
-    } while (!exit);
+        if (exit) {
+          console.log("exiting...");
+        }
+      } while (!exit);
 
-    reloadPage();
-  };
+      reloadPage();
+    };
+  })();
+
+  $(document).ready(() => {
+    // wait 1 sec after page load to start script
+    setTimeout(() => {
+      SmartFarm.init();
+    }, 1000);
+  });
 })();
-
-$(document).ready(() => {
-  SmartFarm.init();
-});
--- UserScript/Scavange.user.js
+++ UserScript/Scavange.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Scavange
-// @version      0.7
+// @version      0.6
 // @description  Auto scavange
 // @author       Victor Garé
 // @match https://*.tribalwars.com.br/*&screen=place&mode=scavenge*
@@ -9,117 +9,125 @@
 // @updateURL   https://github.com/victorgare/tribalwars/raw/master/UserScript/Scavange.user.js
 // @run-at document-end
 // ==/UserScript==
+(function () {
+  "use strict";
 
-function randonTime(superior, inferior) {
-  const numPosibilidades = superior - inferior;
-  const aleat = Math.random() * numPosibilidades;
-  return Math.round(parseInt(inferior) + aleat);
-}
+  function randonTime(superior, inferior) {
+    const numPosibilidades = superior - inferior;
+    const aleat = Math.random() * numPosibilidades;
+    return Math.round(parseInt(inferior) + aleat);
+  }
 
-const Scavange = new (function () {
-  const scavangesWeight = [15, 6, 3, 2];
+  const Scavange = new (function () {
+    const scavangesWeight = [15, 6, 3, 2];
 
-  const getBlockedScavanges = () => {
-    return document.getElementsByClassName("unlock-button").length;
-  };
+    const getBlockedScavanges = () => {
+      return document.getElementsByClassName("unlock-button").length;
+    };
 
-  const getAvailableScavanges = () => {
-    return document.getElementsByClassName("free_send_button");
-  };
+    const getAvailableScavanges = () => {
+      return document.getElementsByClassName("free_send_button");
+    };
 
-  const getScavangeWeight = () => {
-    const blockedScavanges = getBlockedScavanges();
+    const getScavangeWeight = () => {
+      const blockedScavanges = getBlockedScavanges();
 
-    let weightArray = scavangesWeight;
-    if (blockedScavanges > 0) {
-      weightArray = weightArray.slice(0, blockedScavanges * -1);
-    }
+      let weightArray = scavangesWeight;
+      if (blockedScavanges > 0) {
+        weightArray = weightArray.slice(0, blockedScavanges * -1);
+      }
 
-    return weightArray.reduce((item1, item2) => {
-      return item1 + item2;
-    });
-  };
+      return weightArray.reduce((item1, item2) => {
+        return item1 + item2;
+      });
+    };
 
-  const getAvailableTroops = () => {
-    // we want to avoid sendint the paladin in scavange
-    // we wanto to avoid sending CL in scavange
-    const unitsToAvoid = ["knight", "light"];
+    const getAvailableTroops = () => {
+      // we want to avoid sendint the paladin in scavange
+      // we wanto to avoid sending CL in scavange
+      const unitsToAvoid = ["knight", "light"];
 
-    let responseTroops = [];
-    const troops = document.getElementsByClassName("units-entry-all");
+      let responseTroops = [];
+      const troops = document.getElementsByClassName("units-entry-all");
 
-    for (const troop of troops) {
-      var unitType = troop.getAttribute("data-unit");
+      for (const troop of troops) {
+        var unitType = troop.getAttribute("data-unit");
 
-      if (!unitsToAvoid.includes(unitType)) {
-        responseTroops.push({
-          unit: troop.getAttribute("data-unit"),
-          quantity: parseInt(troop.innerHTML.replace("(", "").replace(")", "")),
+        if (!unitsToAvoid.includes(unitType)) {
+          responseTroops.push({
+            unit: troop.getAttribute("data-unit"),
+            quantity: parseInt(
+              troop.innerHTML.replace("(", "").replace(")", "")
+            ),
+          });
+        }
+      }
+
+      return responseTroops;
+    };
+
+    const calculateScavangeTroops = (scavangeWeight, troops) => {
+      const totalWeight = getScavangeWeight();
+
+      const result = [];
+      for (const troop of troops) {
+        const troopsToSend = Math.floor(
+          (troop.quantity * scavangeWeight) / totalWeight
+        );
+
+        result.push({
+          unit: troop.unit,
+          quantityToSend: troopsToSend,
         });
       }
-    }
 
-    return responseTroops;
-  };
+      return result;
+    };
 
-  const calculateScavangeTroops = (scavangeWeight, troops) => {
-    const totalWeight = getScavangeWeight();
-
-    const result = [];
-    for (const troop of troops) {
-      const troopsToSend = Math.floor(
-        (troop.quantity * scavangeWeight) / totalWeight
-      );
-
-      result.push({
-        unit: troop.unit,
-        quantityToSend: troopsToSend,
-      });
-    }
-
-    return result;
-  };
-
-  const sendScavange = (weight, troops, element) => {
-    const troopsToSend = calculateScavangeTroops(weight, troops);
-    for (const troopToSend of troopsToSend) {
-      if (troopToSend.quantityToSend) {
-        var inputs = $(`[name=${troopToSend.unit}]`);
-        inputs.val(troopToSend.quantityToSend.toString()).change();
+    const sendScavange = (weight, troops, element) => {
+      const troopsToSend = calculateScavangeTroops(weight, troops);
+      for (const troopToSend of troopsToSend) {
+        if (troopToSend.quantityToSend) {
+          var inputs = $(`[name=${troopToSend.unit}]`);
+          inputs.val(troopToSend.quantityToSend.toString()).change();
+        }
       }
-    }
 
-    element.click();
-  };
+      element.click();
+    };
 
-  this.init = () => {
-    const troops = getAvailableTroops();
-    const availableScavanges = getAvailableScavanges();
+    this.init = () => {
+      const troops = getAvailableTroops();
+      const availableScavanges = getAvailableScavanges();
 
-    const scavangesUnlocked = scavangesWeight.length - getBlockedScavanges();
+      const scavangesUnlocked = scavangesWeight.length - getBlockedScavanges();
 
-    // only run scavange if all unlocked are available
-    // to prevent from sending wrong number of troops
-    if (availableScavanges.length >= scavangesUnlocked) {
-      for (let index = 0; index < availableScavanges.length; index++) {
-        const weight = scavangesWeight[index];
-        const element = availableScavanges[index];
+      // only run scavange if all unlocked are available
+      // to prevent from sending wrong number of troops
+      if (availableScavanges.length >= scavangesUnlocked) {
+        for (let index = 0; index < availableScavanges.length; index++) {
+          const weight = scavangesWeight[index];
+          const element = availableScavanges[index];
 
-        const delayTime = 3000 + 3000 * index;
-        setTimeout(() => sendScavange(weight, troops, element), delayTime);
+          const delayTime = 3000 + 3000 * index;
+          setTimeout(() => sendScavange(weight, troops, element), delayTime);
+        }
       }
-    }
-  };
+    };
+  })();
+
+  $(document).ready(() => {
+    // wait 1 sec after page load to start script
+    setTimeout(() => {
+      Scavange.init();
+    }, 1000);
+  });
+
+  // reload between 5 and 10 minutes
+  const reloadTime = randonTime(300000, 600000);
+  console.log(`will reload in ${reloadTime / 1000} seconds`);
+  setInterval(function () {
+    console.log("reloading...");
+    location.reload(true);
+  }, reloadTime);
 })();
-
-// reload between 5 and 10 minutes
-const reloadTime = randonTime(300000, 600000);
-console.log(`will reload in ${reloadTime / 1000} seconds`);
-setInterval(function () {
-  console.log("reloading...");
-  location.reload(true);
-}, reloadTime);
-
-$(document).ready(() => {
-  Scavange.init();
-});
